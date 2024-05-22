8d26fd5fad8aa40aea2aa6a0b115206eff0a1111
fishberk <mark.fishman@dci-student.org>
2024-05-22 21:52:54 +0200

    header progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+
 
 function App() {
-  return;
+  return(
   <>
-    {/** Head */}
-    {/** Body */}
-    {/** Footer */}
-  </>;
+    <Header />
+   
+  </>
+  );
 }
 
-export default App;
+export default App
